3c2783b59d14dbc19ccd5e149fc836920ec40077
Mateusz Sieradzki <35726839+sieradzki@users.noreply.github.com>
2023-11-15 19:54:37 +0100

    Hide unseen faces

--- environment/Cube.gd
+++ environment/Cube.gd
@@ -1,19 +1,28 @@
 extends Node
 
-var piece = preload("res://Piece.tscn")
+var Piece = preload("res://Piece.tscn")
 var pieces = []
 @export var size : int = 3
 
+enum FACES { UP, DOWN, RIGHT, LEFT, FRONT, BACK }
+
 func _ready():
 	var positions = get_positions(size)
-	var instance: Node3D = piece.instantiate()
+	print(positions)
+	var instance: Node3D = Piece.instantiate()
 	for x in positions:
 		for y in positions:
 			for z in positions:
-				var cube = instance.duplicate()
-				cube.position = Vector3(x, y, z)
-				pieces.append(cube)
-				add_child(cube)
+				var piece = instance.duplicate()
+				piece.position = Vector3(x, y, z)
+				pieces.append(piece)
+				add_child(piece)
+				piece.get_child(0).get_child(FACES.UP).visible = y == positions[-1]
+				piece.get_child(0).get_child(FACES.DOWN).visible = y == positions[0]
+				piece.get_child(0).get_child(FACES.RIGHT).visible = x == positions[-1]
+				piece.get_child(0).get_child(FACES.LEFT).visible = x == positions[0]
+				piece.get_child(0).get_child(FACES.FRONT).visible = z == positions[0]
+				piece.get_child(0).get_child(FACES.BACK).visible = z == positions[-1]
 
 func get_positions(size):
 	# This makes sure the cube is centered on the scene
